1d160c1ab36d125128213c9d72b45740b3bde5be
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2021-11-04 17:34:31 +0100

    Corretto commit precedente

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 // alert ("Hello World!");
-function myFunction() {
-
+function myFunction() {   
     let x = document.getElementById("myInput");
     let y;
 
     if (x.type === "password") {
       x.type = "text";
-      y = document.classList.remove('fa-eye');
-      y = document.classList.add('fa-eye-slash');
+      y = document.getElementById("show-password").classList.remove('fa-eye');
+      y = document.getElementById("show-password").classList.add('fa-eye-slash');
     } else {
       x.type = "password";
-      y = document.classList.remove('fa-eye-slash');
-      y = document.classList.add('fa-eye')
+      y = document.getElementById("show-password").classList.remove('fa-eye-slash');
+      y = document.getElementById("show-password").classList.add('fa-eye')
     }
 }
