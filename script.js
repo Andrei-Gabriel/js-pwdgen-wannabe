8d27e0bbe4ebedd7e858c6b335235f9a5aa696b8
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2021-11-04 17:31:00 +0100

    Correzione sintassi js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // alert ("Hello World!");
 function myFunction() {
+
     let x = document.getElementById("myInput");
     let y;
 
     if (x.type === "password") {
       x.type = "text";
-      y = document.getElementById("show-password").classList.remove('fa-eye');
-      y = document.getElementById("show-password").classList.add('fa-eye-slash');
+      y = document.classList.remove('fa-eye');
+      y = document.classList.add('fa-eye-slash');
     } else {
       x.type = "password";
-      y = document.getElementById("show-password").classList.remove('fa-eye-slash');
-      y = document.getElementById("show-password").classList.add('fa-eye')
+      y = document.classList.remove('fa-eye-slash');
+      y = document.classList.add('fa-eye')
     }
 }
