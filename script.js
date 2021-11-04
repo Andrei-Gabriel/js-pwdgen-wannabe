9adad4b9b64767cc58585f50dccaea8c6b0eb6cd
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2021-11-04 17:27:48 +0100

    Fine funzione show-or-hide

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 // alert ("Hello World!");
 function myFunction() {
-    var x = document.getElementById("myInput");
+    let x = document.getElementById("myInput");
+    let y;
+
     if (x.type === "password") {
       x.type = "text";
+      y = document.getElementById("show-password").classList.remove('fa-eye');
+      y = document.getElementById("show-password").classList.add('fa-eye-slash');
     } else {
       x.type = "password";
+      y = document.getElementById("show-password").classList.remove('fa-eye-slash');
+      y = document.getElementById("show-password").classList.add('fa-eye')
     }
 }
